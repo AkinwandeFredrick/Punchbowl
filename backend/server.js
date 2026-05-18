1b1eba3622d05611fd7b33969af5e94463b1380b
Falodun Alexander <166916062+Falodun99@users.noreply.github.com>
2026-05-18 14:15:43 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,15 @@
 const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Server running');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 const cors    = require('cors');
 const TelegramBot = require('node-telegram-bot-api');
 const { v4: uuidv4 } = require('uuid');
